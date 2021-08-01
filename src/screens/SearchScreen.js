ad20233ecc38fe795bc68d1b73f7d53c6bd2bed9
Amy Cox <amy_cox@me.com>
2021-08-01 09:31:46 +0100

    Filter results based on price

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,18 +1,22 @@
-import React, {useState, useEffect } from 'react';
+import React, {useState} from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
-import yelp from '../api/yelp';
 import useResults from '../hooks/useResults'
+import ResultsList from '../components/ResultsList'
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
     // console.log(term)
     const [searchApi, results, errorMessage] = useResults();
-    console.log("SEARCH API", searchApi, "RESULTS", results)
+
+    const filterResultsByPrice = (price) => {
+        // price === '$' || '$$' || '$$$'
+        return results.filter(result => {
+            return result.price === price
+        })
+    }
 
    
-
-
     return <View>
         <SearchBar 
             term={term} 
@@ -20,6 +24,11 @@ const SearchScreen = () => {
             onTermSubmit={() => searchApi(term) }/>
             {errorMessage ? <Text>{errorMessage}</Text> : null}
         <Text>We have found {results.length} results </Text>
+        <ResultsList title="Cost Effective" results={filterResultsByPrice("$")}></ResultsList>
+        <ResultsList title="Bit Pricier" results={filterResultsByPrice("$$")}></ResultsList>
+
+        <ResultsList title="Big Spender" results={filterResultsByPrice("$$$")}></ResultsList>
+
     </View>
 }
 
