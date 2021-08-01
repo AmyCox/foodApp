670ca9e4bce335e56287b14948d77cc9efec4ea4
Amy Cox <amy_cox@me.com>
2021-08-01 10:38:38 +0100

    Up to lecture 119

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet,  ScrollView } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import useResults from '../hooks/useResults'
 import ResultsList from '../components/ResultsList'
@@ -17,19 +17,19 @@ const SearchScreen = () => {
     }
 
    
-    return <View>
+    return <>
         <SearchBar 
             term={term} 
             onTermChange={newTerm => setTerm(newTerm)}
             onTermSubmit={() => searchApi(term) }/>
             {errorMessage ? <Text>{errorMessage}</Text> : null}
-        <Text>We have found {results.length} results </Text>
-        <ResultsList title="Cost Effective" results={filterResultsByPrice("$")}></ResultsList>
-        <ResultsList title="Bit Pricier" results={filterResultsByPrice("$$")}></ResultsList>
+            <ScrollView>
+        <ResultsList  title="Cost Effective" results={filterResultsByPrice("$")}></ResultsList>
+        <ResultsList  title="Bit Pricier" results={filterResultsByPrice("$$")}></ResultsList>
 
-        <ResultsList title="Big Spender" results={filterResultsByPrice("$$$")}></ResultsList>
-
-    </View>
+        <ResultsList  title="Big Spender" results={filterResultsByPrice("$$$")}></ResultsList>
+</ScrollView>
+    </>
 }
 
 const styles = StyleSheet.create({});
