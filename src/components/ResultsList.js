5ef879fbc05e5180d4c7b3f39124b34e9e291877
Amy Cox <amy_cox@me.com>
2021-08-02 08:12:50 +0100

    stopped content loading title if there are no results

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,33 +1,52 @@
-import React from 'react';
-import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
-import ResultsDetail from './ResultsDetail';
-import { withNavigation } from 'react-navigation';
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import ResultsDetail from "./ResultsDetail";
+import { withNavigation } from "react-navigation";
 
-const ResultsList = ({title, results, navigation }) => {
-    return <View style={styles.container}>
-        <Text style={styles.title}>{title}</Text>
-        <FlatList showsHorizontalScrollIndicator={false} horizontal={true} data={results} keyExtractor={(result) => result.id} renderItem={({item}) => {
-            return (
-                <TouchableOpacity onPress={()=> navigation.navigate('ResultsShow', {id : item.id})}>
-            <ResultsDetail result={item} />
+const ResultsList = ({ title, results, navigation }) => {
+  if (!results.length) {
+    return null;
+  }
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>{title}</Text>
+      <FlatList
+        showsHorizontalScrollIndicator={false}
+        horizontal={true}
+        data={results}
+        keyExtractor={(result) => result.id}
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity
+              onPress={() =>
+                navigation.navigate("ResultsShow", { id: item.id })
+              }
+            >
+              <ResultsDetail result={item} />
             </TouchableOpacity>
-            )
-        }}>
-
-        </FlatList>
+          );
+        }}
+      ></FlatList>
     </View>
-}
+  );
+};
 
 const styles = StyleSheet.create({
-    title: {
-        fontSize: 18,
-        fontWeight: 'bold', 
-        marginLeft: 15,
-        marginBottom: 5
-    },
-    container: {
-        marginBottom: 10
-    }
-})
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginLeft: 15,
+    marginBottom: 5,
+  },
+  container: {
+    marginBottom: 10,
+  },
+});
 
-export default withNavigation(ResultsList)
+export default withNavigation(ResultsList);
